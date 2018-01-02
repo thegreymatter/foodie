9f0887ef7e7d353a5724110c5302aa3c66463ba9
leeran <leeran@sears.co.il>
2018-01-02 12:15:04 +0200

    first function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,9 @@
+'use strict';
+
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.register = functions.https.onRequest((req, res) => {
+    console.log(req.query);
+
+    res.status(200).send('ok');
+});
