1d69f2cefff0bcd9ad852ee417d1277052603626
leeran <leeran@sears.co.il>
2018-01-02 12:33:39 +0200

    adding new stub user

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,28 @@
 'use strict';
 
 const functions = require('firebase-functions');
+const firebase = require('firebase');
 
-exports.register = functions.https.onRequest((req, res) => {
-    console.log(req.query);
+const firebaseConfig = {
+    apiKey: "AIzaSyAgz4f_lRZGVgE43U3oMc4KQFaYXkXJdyQ",
+    authDomain: "syw-foodie.firebaseapp.com",
+    databaseURL: "https://syw-foodie.firebaseio.com",
+    projectId: "syw-foodie",
+    storageBucket: "syw-foodie.appspot.com",
+    messagingSenderId: "504115679989"
+};
+
+exports.register = functions.https.onRequest(async (req, res) => {
+    console.log(req);
+
+    await firebase.initializeApp(firebaseConfig);
+
+    const user = {
+        id: 1,
+        name: 'Leeran'
+    };
+
+    firebase.database().ref($`/users/{user.id}`).set(user);
 
     res.status(200).send('ok');
 });
